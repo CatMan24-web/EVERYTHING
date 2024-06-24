18b644b3404909d44fd35dc1cefe1f02c5c55084
CatMan24-web <ryanchen02262011@gmail.com>
2024-06-24 12:16:14 +0100

    Update python.py

--- python.py
+++ python.py
@@ -8,9 +8,9 @@ t = 0        # time
 dt = 0.01    # time interval Graph
 
 
-scene = canvas(title="1D Motion", width=800, height=600, x=0, y=0, center=vec(0, 0.1, 0), background=vec(0, 0.6, 0.6))
+scene = canvas(title="1D Motion", width=800, height=600, x=0, y=0, center=vec(0, 0.1, 0), #background=vec(0, 0.6, 0.6))
 floor = box(pos=vec(0, 0, 0), size=vec(L, 0.1*size, 0.5*L), color=color.blue)
-cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), size=vec(size, size, size), color=color.red, v=vec(v, 0, 0))
+cube = box(pos=vec(-0.5*L + 0.5*size, 0.55*size, 0), size=vec(size, size, size), color=color.red #v=vec(v, 0, 0))
 
 
 #"""
